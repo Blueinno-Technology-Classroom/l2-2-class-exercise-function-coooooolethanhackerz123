061a898b0e7e271dfd8b63d1cb9c0b066f9be739
cooooooolethanhackerz22113 <wangethanhk@gmail.com>
2024-01-27 11:49:06 +0800

    ez

--- main.py
+++ main.py
@@ -4,28 +4,43 @@ Q1:
 '''
 
 # TODO: Write your code here
+def greet(human):
+   print(f'Hi, {human}!')
 
+greet('Alice')
+greet('Bob')
 ##################################################
 '''
 Q2:
 '''
 
 # TODO: Write your code here
-
+def greet(name1,name2):
+   print(f'Hi, {name1} and {name2}')
+greet(name2='Bob', name1='Alex')
 ##################################################
 '''
 Q3:
 '''
 
 # TODO: Write your code here
+def find_rect_area(width, height):
+    print(width*height)
 
+find_rect_area(width=12.3, height=45.6)
 ##################################################
 '''
 Q4:
 '''
 
 # TODO: Write your code here
-
+def bought(animal):
+    print(f'Bought me a {animal} and the {animal} pleased me, I fed my {animal} under the yonder tree. Cat goes fiddle-i-fee.')
+    
+bought('cat')
+bought('hen')
+bought('duck')
+bought('goose')
 ##################################################
 '''
 Q5:
